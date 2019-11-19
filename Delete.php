1aa90d7d9f83ff53df4dcd3ccaf26441dd3aebf2
ars3na1 <32192217+ars3na1@users.noreply.github.com>
2019-11-19 22:11:04 +0200

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,18 +1,22 @@
 <?php
 include 'BD.php';
 
-if(isset($_GET["id"])) {
-$article_id = mysqli_escape_string($con, $_GET["id"]);
-	if (is_numeric($article_id) && $article_id > 0){
-$delete_a=mysqli_query($con, "DELETE FROM `blog articles` WHERE id='$article_id'");
+if (isset($_GET["id"])) {
+$art_id = $_GET["id"];
+	if (is_numeric($art_id) && $art_id > 0){
+$stmt = $con->prepare("DELETE FROM `blog articles` WHERE id = ?");
+$stmt->bind_param('i', $art_id);
+$stmt->execute();
+$stmt->close();
 	}
-} else if(isset($_GET["Category"])) {
-$category = mysqli_escape_string($con, $_GET["Category"]);
-	if(is_string($category) && strlen($category) > 0) {
-$delete_c=mysqli_query($con, "DELETE FROM `blog articles` WHERE category='$category'");
+} else if (isset($_GET["Category"])) {
+$cat = $_GET["Category"];
+	if(is_string($cat) && strlen($cat) > 0) {
+$stmt = $con->prepare("DELETE FROM `blog articles` WHERE category = ?");
+$stmt->bind_param('s', $cat);
+$stmt->execute();
 	}
 }
-
 if(mysqli_affected_rows($con)) {
 	header('Location: index.php');
 }
